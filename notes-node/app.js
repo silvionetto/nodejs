406bd9dfeaeff025d0f4414bb63764d2b1ef66f4
Silvio Netto <silvio.netto@gmail.com>
2017-10-28 17:17:38 +0200

    Using NPM

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,6 +3,7 @@ console.log('Starting app');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
 var user = os.userInfo();
 console.log(user.username);
@@ -14,3 +15,9 @@ console.log(res);
 
 var con = notes.concat(1, 5);
 console.log(con);
+
+console.log(_.isString('Silvio'));
+console.log(_.isString(1));
+
+var filteredArray = _.uniq(['Silvio','Netto', 1,'Silvio', 1, 2, 3, 4, 5]);
+console.log(filteredArray);
